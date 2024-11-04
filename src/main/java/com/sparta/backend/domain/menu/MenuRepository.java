145a236d165a8dcde03d0ffa0ddce71d699c8532
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2024-11-04 15:17:54 +0900

    [Feat] 가게 단건 조회 기능 구현 (#15)

--- src/main/java/com/sparta/backend/domain/menu/MenuRepository.java
+++ src/main/java/com/sparta/backend/domain/menu/MenuRepository.java
@@ -1,4 +1,9 @@
 package com.sparta.backend.domain.menu;
 
-public class MenuRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    List<Menu> findByStoreIdAndStatus(final Long storeId, final MenuStatus status);
 }
--- src/main/java/com/sparta/backend/domain/store/Category.java
+++ src/main/java/com/sparta/backend/domain/store/Category.java
@@ -1,5 +1,14 @@
 package com.sparta.backend.domain.store;
 
+import java.util.Arrays;
+
 public enum Category {
-    KOREA, JAPAN, CHINA
+    KOREA, JAPAN, CHINA;
+
+    public static Category from(final String category) {
+        return Arrays.stream(values())
+                .filter(v -> v.name().equalsIgnoreCase(category))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("이 카테고리는 존재하지 않습니다."));
+    }
 }
