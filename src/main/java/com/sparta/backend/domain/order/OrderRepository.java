999cd4cf936eb4273aea1d177307c3d903bd3fcc
Jisoo.R <jisuryu0527@gmail.com>
2024-11-04 19:27:47 +0900

    Feat: 주문 생성 기능 구현

--- src/main/java/com/sparta/backend/domain/order/OrderRepository.java
+++ src/main/java/com/sparta/backend/domain/order/OrderRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.backend.domain.order;
 
-public class OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+
 }
