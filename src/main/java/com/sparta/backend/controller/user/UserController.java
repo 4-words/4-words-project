c19f8685f6883298334afc7d23fd999bc3236db6
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2024-11-05 22:23:26 +0900

    유저 이미지 변경, 메뉴 이미지 변경 기능 구현 (#42)
    * [Feat] #36 유저 프로필만 변경 기능 구현
    
    * [Feat] #36 이미지를 제외한 유저 프로필 수정 기능 구현
    
    * [Feat] #36 가게 폐업 시 비활성화 기능 구현
    
    * [Feat] #36 메뉴 저장 시 이미지 저장 기능 추가
    
    * [Feat] #36 import 추가
    
    * [Refactor] #36 Menu 저장 로직 리팩토링

--- src/main/java/com/sparta/backend/controller/user/UserController.java
+++ src/main/java/com/sparta/backend/controller/user/UserController.java
@@ -2,23 +2,49 @@ package com.sparta.backend.controller.user;
 
 import com.sparta.backend.common.resolver.AuthenticationUserId;
 import com.sparta.backend.controller.user.dto.ProfileResponseDto;
+import com.sparta.backend.controller.user.dto.UserUpdateRequest;
 import com.sparta.backend.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/profile")
+@RequestMapping("/profile/user")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user/myself")
-    public ResponseEntity<ProfileResponseDto> getProfile (@AuthenticationUserId Long id) {
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Void> updateImage(
+            @PathVariable final Long userId,
+            @RequestPart final MultipartFile image,
+            @AuthenticationUserId final Long loginId
+    ) {
+        userService.updateImage(userId, image, loginId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> updateProfile(
+            @PathVariable final Long userId,
+            @RequestBody final UserUpdateRequest req,
+            @AuthenticationUserId final Long loginId
+    ) {
+        userService.updateProfile(userId, req, loginId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/myself")
+    public ResponseEntity<ProfileResponseDto> getProfile(@AuthenticationUserId Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getProfile(id));
     }
 }
--- src/main/java/com/sparta/backend/service/user/UserService.java
+++ src/main/java/com/sparta/backend/service/user/UserService.java
@@ -1,23 +1,55 @@
 package com.sparta.backend.service.user;
 
+import static com.sparta.backend.common.ErrorCodes.USER_NOT_FOUND;
+
+import com.sparta.backend.client.S3FileUploader;
 import com.sparta.backend.common.ApplicationException;
+import com.sparta.backend.common.ErrorCodes;
 import com.sparta.backend.controller.user.dto.ProfileResponseDto;
+import com.sparta.backend.controller.user.dto.UserUpdateRequest;
 import com.sparta.backend.domain.user.User;
 import com.sparta.backend.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import static com.sparta.backend.common.ErrorCodes.USER_NOT_FOUND;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final S3FileUploader fileUploader;
+
+    @Transactional
+    public void updateImage(final Long userId, final MultipartFile image, final Long loginId) {
+        final User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ApplicationException(USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+
+        if (!user.isOwner(loginId)) {
+            throw new ApplicationException(ErrorCodes.USER_NOT_OWNER, HttpStatus.FORBIDDEN);
+        }
+
+        final String imageUrl = fileUploader.uploadFiles(image);
+        user.update(imageUrl);
+    }
+
+    @Transactional
+    public void updateProfile(final Long userId, final UserUpdateRequest req, final Long loginId) {
+        final User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ApplicationException(USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+
+        if (!user.isOwner(loginId)) {
+            throw new ApplicationException(ErrorCodes.USER_NOT_OWNER, HttpStatus.FORBIDDEN);
+        }
+
+        user.updateProfile(req.name(), req.password(), req.email(), req.address(), req.role());
+    }
 
     public ProfileResponseDto getProfile(Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ApplicationException(USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ApplicationException(USER_NOT_FOUND, HttpStatus.NOT_FOUND));
         ProfileResponseDto profileResponseDto = new ProfileResponseDto(user);
         return profileResponseDto;
     }
