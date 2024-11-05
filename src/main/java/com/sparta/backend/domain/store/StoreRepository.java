1a641ecafe50ee7f30826cf04d8dc6f4d5ca167a
yuseonjun <seonjun0906@gmail.com>
2024-11-05 12:46:43 +0900

    [Refactor] #30 코드 리팩토링

--- src/main/java/com/sparta/backend/domain/store/StoreRepository.java
+++ src/main/java/com/sparta/backend/domain/store/StoreRepository.java
@@ -3,7 +3,7 @@ package com.sparta.backend.domain.store;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<Store, Long> {
+public interface StoreRepository extends JpaRepository<Store, Long>, StoreCustomRepository {
 
     Optional<Store> findByIdAndStatus(final Long storeId, final StoreStatus status);
 }
