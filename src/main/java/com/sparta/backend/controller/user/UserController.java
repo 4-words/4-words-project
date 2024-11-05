4e94cb73e0229dae5613bfbe4f52967aea08a66c
gyeonwoo-jerry <gyeonwoo2916@gmail.com>
2024-11-05 16:28:36 +0900

    [Feat] 유저 프로필 조회 구현 (#32)
    * [Feat] 유저 프로필 조회 구현
    - 유저 프로필 리스폰스 DTO 추가
    
    * [Feat] 유저 프로필 조회 구현
    - 유저 프로필 리스폰스 DTO 추가
    - PR 코멘트부분 수정
    (예외처리, Response Dto Getter제거)
    
    ---------
    
    Co-authored-by: gyeonwoo-jerry <gyeonwoo2916@google.com>
    Co-authored-by: dameun0527 <jisuryu0527@gmail.com>

--- src/main/java/com/sparta/backend/controller/user/UserController.java
+++ src/main/java/com/sparta/backend/controller/user/UserController.java
@@ -1,9 +1,24 @@
 package com.sparta.backend.controller.user;
 
+import com.sparta.backend.common.resolver.AuthenticationUserId;
+import com.sparta.backend.controller.user.dto.ProfileResponseDto;
+import com.sparta.backend.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/profile")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/user/myself")
+    public ResponseEntity<ProfileResponseDto> getProfile (@AuthenticationUserId Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getProfile(id));
+    }
 }
--- src/main/java/com/sparta/backend/service/user/UserService.java
+++ src/main/java/com/sparta/backend/service/user/UserService.java
@@ -1,9 +1,24 @@
 package com.sparta.backend.service.user;
 
+import com.sparta.backend.common.ApplicationException;
+import com.sparta.backend.controller.user.dto.ProfileResponseDto;
+import com.sparta.backend.domain.user.User;
+import com.sparta.backend.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import static com.sparta.backend.common.ErrorCodes.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public ProfileResponseDto getProfile(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ApplicationException(USER_NOT_FOUND, HttpStatus.NOT_FOUND));
+        ProfileResponseDto profileResponseDto = new ProfileResponseDto(user);
+        return profileResponseDto;
+    }
 }
