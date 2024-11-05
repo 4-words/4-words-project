cfdfe8524d6c60c55408d9957c1fdfd532086251
Jisoo.R <jisuryu0527@gmail.com>
2024-11-05 16:47:26 +0900

    주문 상태 코드 일부 삭제
    * 불확실한 코드라 삭제함

--- src/main/java/com/sparta/backend/domain/order/OrderStatus.java
+++ src/main/java/com/sparta/backend/domain/order/OrderStatus.java
@@ -1,20 +1,5 @@
 package com.sparta.backend.domain.order;
 
-import com.sparta.backend.common.ApplicationException;
-
 public enum OrderStatus {
-    WAITING, COOKING, DELIVERING, COMPLETE
-
-    public OrderStatus getNextStatus() {
-        switch (this) {
-            case WAITING:
-                return COOKING;
-            case COOKING:
-                return DELIVERING;
-            case DELIVERING:
-                return COMPLETE;
-            default:
-                throw new ApplicationException();
-        }
+    WAITING, COOKING, DELIVERING, COMPLETE;
     }
-}
