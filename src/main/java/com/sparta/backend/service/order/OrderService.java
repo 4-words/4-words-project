a327a0a8c28c7d78467f2919401cbd45e008ddf0
Jisoo.R <jisuryu0527@gmail.com>
2024-11-05 09:29:53 +0900

    Feat: 주문 생성 기능 로직 예외 처리
    * GlobalException ErrorCode 추가
    * STORE_CLOSED: 가게의 영업 시간 외 주문 불가능
    * MIN_ORDER_PRICE_NOT_MET: 가게 최소 주문 금액보다 적을 시 주문 불가능

--- src/main/java/com/sparta/backend/service/order/OrderService.java
+++ src/main/java/com/sparta/backend/service/order/OrderService.java
@@ -1,5 +1,6 @@
 package com.sparta.backend.service.order;
 
+import com.sparta.backend.common.ApplicationException;
 import com.sparta.backend.controller.order.dto.CreateOrderRequest;
 import com.sparta.backend.controller.order.dto.CreateOrderResponse;
 import com.sparta.backend.domain.menu.Menu;
@@ -12,9 +13,13 @@ import com.sparta.backend.domain.store.StoreRepository;
 import com.sparta.backend.domain.user.User;
 import com.sparta.backend.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+
+import static com.sparta.backend.common.ErrorCodes.MIN_ORDER_PRICE_NOT_MET;
+import static com.sparta.backend.common.ErrorCodes.STORE_CLOSED;
 
 @Service
 @RequiredArgsConstructor
@@ -30,10 +35,10 @@ public class OrderService {
         User user = userRepository.findById(id).orElseThrow();
         LocalDateTime now = LocalDateTime.now();
         if(now.isBefore(store.getOpenedAt()) || now.isAfter(store.getClosedAt())) {
-            throw new StoreClosedException();
+            throw new ApplicationException(STORE_CLOSED, HttpStatus.FORBIDDEN);
         }
         if(request.getTotalPrice() < store.getMinOrderPrice()){
-            throw new MinOrderPriceException("");
+            throw new ApplicationException(MIN_ORDER_PRICE_NOT_MET, HttpStatus.BAD_REQUEST);
         }
         Order order = new Order(user,store,menu, request.getType(), request.getTotalPrice(), OrderStatus.WAITING);
         Order savedOrder = orderRepository.save(order);
