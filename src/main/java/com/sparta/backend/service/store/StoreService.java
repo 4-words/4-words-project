8c22593a025941c8dab06db28377871154f736b1
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2024-11-05 12:53:59 +0900

    [Refactor] #30 코드 리팩토링 (#35)

--- src/main/java/com/sparta/backend/service/store/StoreService.java
+++ src/main/java/com/sparta/backend/service/store/StoreService.java
@@ -24,6 +24,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -75,7 +76,6 @@ public class StoreService {
             final int page,
             final int limit
     ) {
-        return storeRepository.retrieveByCategory(type,
-                PageRequest.of(page, limit));
+        return storeRepository.retrieveByCategory(type, PageRequest.of(page, limit));
     }
 }
