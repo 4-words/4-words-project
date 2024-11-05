108c5444b8290e1a691e4987716036934a490c49
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2024-11-05 10:56:46 +0900

    [Feat] #26 가게 수정 로직 구현 (#29)

--- src/main/java/com/sparta/backend/domain/store/StoreRepository.java
+++ src/main/java/com/sparta/backend/domain/store/StoreRepository.java
@@ -1,6 +1,9 @@
 package com.sparta.backend.domain.store;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    Optional<Store> findByIdAndStatus(final Long storeId, final StoreStatus status);
 }
