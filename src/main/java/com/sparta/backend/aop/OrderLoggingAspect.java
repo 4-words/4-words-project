0aa90c60eabd995029aed18917e34fb4e11c71d6
Jisoo.R <jisuryu0527@gmail.com>
2024-11-06 12:19:29 +0900

    Feat: 주문 상태 변경 AOP 기능 추가
    * 주문 상태가 변경되면 가게 id, 주문 id , 주문 상태, 요청 시각이 로그에 나타나도록 함.

--- src/main/java/com/sparta/backend/aop/OrderLoggingAspect.java
+++ src/main/java/com/sparta/backend/aop/OrderLoggingAspect.java
@@ -1,12 +1,18 @@
 package com.sparta.backend.aop;
+import com.sparta.backend.common.ApplicationException;
+import com.sparta.backend.common.ErrorCodes;
 import com.sparta.backend.controller.order.dto.CreateOrderResponse;
 import com.sparta.backend.domain.order.Order;
+import com.sparta.backend.domain.order.OrderRepository;
+import com.sparta.backend.domain.order.OrderStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -16,14 +22,34 @@ import java.time.LocalDateTime;
 @Component
 public class OrderLoggingAspect {
 
+    private final OrderRepository orderRepository;
+
+    public OrderLoggingAspect(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @AfterReturning(pointcut = "execution(* com.sparta.backend.service.order.OrderService.createOrder(..))",
             returning = "response")
     public void logNewOrder(JoinPoint joinPoint, CreateOrderResponse response) {
-        Long storeId = response.getId(); // 가게 ID가 아니라 주문 ID를 가져옴
-        String storeName = response.getStoreName();
+        Long storeId = response.getStoreId();
         Long orderId = response.getId();
         LocalDateTime createdAt = response.getCreatedAt();
 
-        log.info("새로운 주문이 생성되었습니다. 주문 요청 시각: {}, 가게 ID: {}, 주문 ID: {}", createdAt, storeName, orderId);
+        log.info("새로운 주문이 생성되었습니다. 주문 요청 시각: {}, 가게 ID: {}, 주문 ID: {}", createdAt, storeId, orderId);
+    }
+
+    @AfterReturning("execution(* com.sparta.backend.service.order.OrderService.acceptOrder(..))")
+    public void logOrderStatusChange(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Long orderId = (Long) args[0];
+        LocalDateTime statusChangedAt = LocalDateTime.now();
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(()-> new ApplicationException(ErrorCodes.ORDER_NOT_FOUND, HttpStatus.NOT_FOUND));
+        Long storeId = order.getStore().getId();
+        OrderStatus newStatus = order.getStatus();
+
+        log.info("주문 상태가 변경되었습니다. 변경 요청 시각: {}, 가게 ID: {}, 주문 ID: {}, 변경된 상태: {}"
+                , statusChangedAt, storeId, orderId, newStatus);
+
     }
 }
