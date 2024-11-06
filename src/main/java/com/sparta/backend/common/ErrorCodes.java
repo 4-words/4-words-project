a1b4a9aa0f936d005567a74731e1e3830f2cda6d
gyeonwoo-jerry <gyeonwoo2916@google.com>
2024-11-06 11:09:34 +0900

    [Feat] 주문 수락 기능 구현 rebase후 첫 merge

--- src/main/java/com/sparta/backend/common/ErrorCodes.java
+++ src/main/java/com/sparta/backend/common/ErrorCodes.java
@@ -25,10 +25,9 @@ public enum ErrorCodes {
     STORE_NOT_FOUND("가게가 존재하지 않습니다.", 4003L),
     STORE_NOT_OWNER("가게의 사장이 아닙니다.", 4004L),
     MENU_NOT_FOUND("해당 메뉴를 찾을 수 없습니다.", 5001L),
-
+    DELIVERY_NOT_COMPLETE("배달이 아직 완료되지 않았습니다.", 6001L),
     ORDER_NOT_FOUND("주문이 존재하지 않습니다.", 7001L),
     INVALID_STATUS("유효하지 않는 상태입니다.", 7002L);
-
 
     public final @NotNull String message;
     public final @NotNull Long code;
